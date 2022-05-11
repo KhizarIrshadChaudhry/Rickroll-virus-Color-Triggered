0dedd120a350f6c3bb2b5dcaf2dc81b93f0bdc05
-1 <k.irshadch@gmail.com>
2022-05-11 20:21:35 +0200

    Create main.py
    Forgot to add the main.py file, sorry guys lol

---
+++ main.py
@@ -0,0 +1,24 @@
+import pyautogui
+from playsound import playsound
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+
+def main():
+    for i in range(50):
+        keyboard.press(Key.media_volume_up)
+        keyboard.release(Key.media_volume_up)
+    playsound("alol.wav")
+
+
+while True:
+    try:
+        while True:
+
+            if pyautogui.pixel(pyautogui.position().x, pyautogui.position().y)[0] > 130 and \
+                    pyautogui.pixel(pyautogui.position().x, pyautogui.position().y)[1] > 130 and \
+                    pyautogui.pixel(pyautogui.position().x, pyautogui.position().y)[2] < 60:
+                main()
+    except:
+        pass
